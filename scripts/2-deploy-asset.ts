95c939a8b4f20492132931178fdd2f84813334d3
tubleronchik <m.bystramovich@gmail.com>
2022-11-29 03:00:21 +0300

    add auditor address to asset deploying script

--- scripts/2-deploy-asset.ts
+++ scripts/2-deploy-asset.ts
@@ -1,5 +1,7 @@
+import { Address } from "everscale-inpage-provider";
 export async function deployAsset() {
     const signer = (await locklift.keystore.getSigner("0"))!;
+    const zeroAddress = new Address("0:0000000000000000000000000000000000000000000000000000000000000000");
     const { contract: mito, tx } = await locklift.factory.deployContract({
       contract: "ERC20Burnable",
       publicKey: signer.publicKey,
@@ -10,7 +12,8 @@ export async function deployAsset() {
         _name: "MITO",
         _symbol: "MITO",
         _decimals: 18,
-        _start_count: 100 * 10 ^ 18
+        _start_count: 100 * 10 ^ 18,
+        _auditor: zeroAddress
       },
       value: locklift.utils.toNano(3),
     });
